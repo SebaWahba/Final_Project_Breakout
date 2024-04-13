707a89ba357eb0a8113c578e3329eff72d8e1819
ghadasherif <135965326+ghadasherif@users.noreply.github.com>
2024-04-14 01:41:07 +0200

    Add files via upload

---
+++ Break_out_Project/blocks.h
@@ -0,0 +1,10 @@
+#ifndef BLOCKS_H
+#define BLOCKS_H
+#include <QGraphicsRectItem>
+class Block: public QGraphicsRectItem{
+public:
+    // constructor
+    Block(QGraphicsItem* parent=NULL);
+
+};
+#endif // BLOCKS_H
